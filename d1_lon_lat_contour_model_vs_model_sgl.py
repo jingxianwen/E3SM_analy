1c67c34ccbb064960053d076a642264e85bcd842
jingxianwen <jingxianwen@live.cn>
2020-06-05 14:56:46 -0700

    plot ssa g

--- d1_lon_lat_contour_model_vs_model_sgl.py
+++ d1_lon_lat_contour_model_vs_model_sgl.py
@@ -21,18 +21,18 @@ from get_parameters import get_area_mean_min_max
 
 #def lon_lat_contour_model_vs_model(varnm,season,scale_ctl,scale_exp,table):
 # data path
-ctl_name="standardref" #os.environ["ctl_name"]
-exp_name="modifiedref" #os.environ["exp_name"]
-fpath_ctl='/global/cscratch1/sd/xianwen/E3SM_simulations/E3SM_v2_alpha_AMIP_RRTMG_UMRad_tau_ice.ne30_ne30.cori-knl/archive/remap_180x360_orig/'
-fpath_exp='/global/cscratch1/sd/xianwen/E3SM_simulations/E3SM_v2_alpha_AMIP_RRTMG_UMRad_tau_ice.ne30_ne30.cori-knl/archive/remap_180x360_UMRad/'
+ctl_name="standard" #os.environ["ctl_name"]
+exp_name="modified" #os.environ["exp_name"]
+fpath_ctl='/global/cscratch1/sd/xianwen/E3SM_simulations/E3SM_v2_alpha_AMIP_RRTMG_UMRad_startover.ne30_ne30.cori-knl/archive/remap_180x360_orig/'
+fpath_exp='/global/cscratch1/sd/xianwen/E3SM_simulations/E3SM_v2_alpha_AMIP_RRTMG_UMRad_startover.ne30_ne30.cori-knl/archive/remap_180x360_UMRad/'
 
 #fpath_exp="../../E3SM_output/E3SM_coupled_restart_20TR_Yr2000-Scat.Year2000_2014/climo/"
  
 #f1=fpath_ctl+"E3SMv2_offline_ICEFLAG1_full2_noEmis_climo_ANN.nc"
 #f2=fpath_exp+"E3SMv2_offline_ICEFLAG1_full2_noEmis_climo_ANN.nc"
 #f1=fpath_ctl+"E3SM_v2_alpha_AMIP_RRTMGP.ne30_ne30.cori-knl.cam.h0.0001-01-01-00000.nc"
-f1=fpath_ctl+"E3SM_v2_alpha_AMIP_RRTMG_UMRad_tau_ice.ne30_ne30.cori-knl.cam.h0.2000-01-01-00000.nc"
-f2=fpath_exp+"E3SM_v2_alpha_AMIP_RRTMG_UMRad_tau_ice.ne30_ne30.cori-knl.cam.h0.2000-01-01-00000.nc"
+f1=fpath_ctl+"E3SM_v2_alpha_AMIP_RRTMG_UMRad_startover.ne30_ne30.cori-knl.cam.h0.2000-01-01-00000.nc"
+f2=fpath_exp+"E3SM_v2_alpha_AMIP_RRTMG_UMRad_startover.ne30_ne30.cori-knl.cam.h0.2000-01-01-00000.nc"
 #f2=fpath_exp+"E3SM_coupled_restart_20TR_Yr2000-Scat.Year2000_2014_climo_ANN.nc"
 
 #f1=fpath_ctl+"solar_TSIS_cesm211_standard-ETEST-f19_g17-ens1.cam.h0.0001-01.nc"
@@ -48,20 +48,21 @@ lon=file_ctl.variables["lon"]
 lev=file_ctl.variables["lev"]
 
 #varnm="FSSDCLRS14"
-varnm="LWCF"
+varnm="G_ICE"
 #varnm_off="LWCF_OFF"  #offline computation
-units=r"W/m$^2$"
+#units=r"W/m$^2$"
+units=""
 figure_name="lat_lon_"+varnm+"_RRTMG_"+exp_name+"-"+ctl_name+"_UMRad_startover.png"
 #figure_name="lat_lon_"+varnm+"500mb_"+exp_name+"-"+ctl_name+".png"
 
 #lev250=np.min(np.where(lev[:]>250.))
-#lev500=np.min(np.where(lev[:]>500.))
+lev500=np.min(np.where(lev[:]>500.))
 
 # read data and calculate mean/min/max
-#dtctl=file_ctl.variables[varnm][:,lev500,:,:] #*scale_ctl
-#dtexp=file_exp.variables[varnm][:,lev500,:,:] #*scale_exp
-dtctl=file_ctl.variables[varnm][:,:,:] #*scale_ctl
-dtexp=file_exp.variables[varnm][:,:,:] #*scale_exp
+dtctl=file_ctl.variables[varnm][:,lev500,:,:] #*scale_ctl
+dtexp=file_exp.variables[varnm][:,lev500,:,:] #*scale_exp
+#dtctl=file_ctl.variables[varnm][:,:,:] #*scale_ctl
+#dtexp=file_exp.variables[varnm][:,:,:] #*scale_exp
 dtdif=dtexp[:,:,:]-dtctl[:,:,:]
 stats_ctl=get_area_mean_min_max(dtctl[:,:,:],lat[:])
 stats_exp=get_area_mean_min_max(dtexp[:,:,:],lat[:])
@@ -106,15 +107,16 @@ for i in range(0,3):
     levels = None
     norm = None
     if i != 2:
-        cnlevels=np.array([0,10,20,30,40,50,60]) #parameters["contour_levs"]
+        #cnlevels=np.array([0,10,20,30,40,50,60]) #parameters["contour_levs"]
         #cnlevels=np.arange(125,300,20)
         #cnlevels=np.arange(70,420,30)
         #cnlevels=np.arange(20,150,10)
         #cnlevels=np.arange(0,80,8)
+        cnlevels=np.arange(0,1,0.1)
     else:
         #cnlevels=np.arange(-9,10,1.5)
         cnlevels=np.arange(-7,8,1)
-        #cnlevels=np.arange(-0.8,1.0,0.2)
+        cnlevels=np.arange(-0.8,1.0,0.2)
         #cnlevels=np.arange(0.0,3.0,0.2)
         #cnlevels=np.array([-4,-3.5,-3,-2.5,-2,-1.5,-1.,-0.5,0.5,1.,1.5,2.,2.5,3.,3.5,4.]) #parameters["diff_levs"]
 
