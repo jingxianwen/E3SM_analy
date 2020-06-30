af3a8187eb99899f0bf94d1d479f2c9a7ad5bb96
jingxianwen <jingxianwen@live.cn>
2020-06-30 10:20:21 -0700

    modify yaxis range

--- d4_zonal_mean_model_vs_model.py
+++ d4_zonal_mean_model_vs_model.py
@@ -264,7 +264,7 @@ ax2.set_title("Differences (Scat - Abs)",fontsize=14) #+var_long_name,fontsize=1
 ax2.set_ylabel(units,fontsize=14)
 ax2.set_xlabel("Latitude",fontsize=14)
 ax2.set_xlim(-90,90)
-ax2.set_ylim(-1.2,0.8)
+ax2.set_ylim(-1.2,0.9)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 
