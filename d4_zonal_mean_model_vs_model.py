1c888e30258eae50c0aea64382006b919c4dae50
jingxianwen <jingxianwen@live.cn>
2020-07-08 20:27:57 -0700

    plot surface air temperature

--- d4_zonal_mean_model_vs_model.py
+++ d4_zonal_mean_model_vs_model.py
@@ -31,8 +31,8 @@ from get_parameters import get_area_mean_min_max
 # data path
 ctl_name="Abs" #os.environ["ctl_name"]
 exp_name="Scat" #os.environ["exp_name"]
-ctl_pref="AMIP_RRTMG_UMRad_abs.ne30_ne30.cori-knl"
-exp_pref="AMIP_RRTMG_UMRad_scat.ne30_ne30.cori-knl"
+ctl_pref="CMIP_RRTMG_UMRad_abs.ne30_ne30.cori-knl"
+exp_pref="CMIP_RRTMG_UMRad_scat.ne30_ne30.cori-knl"
 
 fpath_ctl="/global/cscratch1/sd/xianwen/E3SM_simulations/"+ctl_pref+"/archive/climo/yby/"
 fpath_exp="/global/cscratch1/sd/xianwen/E3SM_simulations/"+exp_pref+"/archive/climo/yby/"
@@ -42,10 +42,10 @@ months_all=["01","02","03","04","05","06","07","08","09","10","11","12"]
 
 var_group_todo=1
 # variable group 1:
-varnms=np.array(["TS"])
+varnms=np.array(["TREFHT"])
 #varnms=np.array(["FSNTOA","FSNS","TS"])
-var_long_name="Surface Temperature"
-figure_name="Surface_Temperature_zonal_ANN_AMIP_scat_vs_abs"
+var_long_name="Surface air Temperature"
+figure_name="Surface_air_Temperature_zonal_ANN_CMIP_scat_vs_abs"
 units="K"
 #var_long_name="Surface Net SW"
 #figure_name="Surface_Net_SW_zonal_ANN"
@@ -238,7 +238,7 @@ ax1.plot(lat[:],means_ctl_JJA[0,:],color="darkorange",lw=2,ls="-",label="JJA")
 #ax1.plot(lat[:],means_exp[0,:],color="k",lw=2,ls=":",label="TSIS")
 #ax1.legend(loc="upper left",fontsize=12)
 ax1.legend(fontsize=12)
-ax1.set_title("Surface Temperature (Abs)",fontsize=14)
+ax1.set_title("Surface Air Temperature (Abs)",fontsize=14)
 ax1.set_ylabel(units,fontsize=14)
 #ax1.set_xlabel("Latitude",fontsize=14)
 ax1.set_xlim(-90,90)
